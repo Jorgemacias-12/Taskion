ce3391159238e9ac6eb9ed16be47448f28451543
Jorge Macias <jorge.macias7905@alumnos.udg.mx>
2023-11-16 06:11:18 -0600

    Add: index view
    	This is the landing page view of the / route

---
+++ views/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+  <?php require_once 'components/styles.php' ?>
+  <link rel="stylesheet" href="/public/css/index.css">
+</head>
+<body>
+  <?php require_once 'components/header.php' ?>
+  <h1>Ola po'</h1>
+</body>
+</html>
