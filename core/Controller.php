1276c451060cc502b9033c448f9c660d8199563f
Jorge Macias <jorge.macias7905@alumnos.udg.mx>
2023-11-16 01:39:40 -0600

    Add: Controller.php
    	Controller reponsability is provide a connection between model
    	and view.

---
+++ core/Controller.php
@@ -0,0 +1,20 @@
+<?php
+class Controller
+{
+  protected function model($model)
+  {
+    require_once '../models/' . $model . '.php';
+    return new $model();
+  }
+
+  protected function view($view, $data = [])
+  {
+    require_once '../views/' . $view . '.php';
+  }
+
+  protected function redirect($path)
+  {
+    header('Location: ' . $path);
+    exit;
+  }
+}
