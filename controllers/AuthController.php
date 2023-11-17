68d89c6e2b65ea3684ccef2a7fa8a1b0febaa9dd
Jorge Macias <jorge.macias7905@alumnos.udg.mx>
2023-11-17 07:03:40 -0600

    Update: AuthController.php
    	Add getLogin and doLogin methods for /login route

--- controllers/AuthController.php
+++ controllers/AuthController.php
@@ -3,5 +3,13 @@
 require_once 'core/Controller.php';
 
 class AuthController extends Controller {
-  
+  public function getLogin()
+  {
+    $this->view('login', ['showHeader' => true]);
+  }
+
+  public function doLogin()
+  {
+
+  }
 }
