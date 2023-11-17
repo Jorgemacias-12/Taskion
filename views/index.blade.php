1aba479d73fb81c029395869c936f106c51006d0
Jorge Macias <jorge.macias7905@alumnos.udg.mx>
2023-11-17 06:18:40 -0600

    Update: index.blade.php
    	Use base blade view, and later add landing page content

--- views/index.blade.php
+++ views/index.blade.php
@@ -1,14 +1,11 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title>
-  <?php require_once 'components/styles.php' ?>
+@extends('base')
+
+@section('title', 'Inicio')
+
+@push('styles')
   <link rel="stylesheet" href="/public/css/index.css">
-</head>
-<body>
-  <?php require_once 'components/header.php' ?>
-  <h1>Ola po'</h1>
-</body>
-</html>
+@endpush
+
+@section('content')
+
+@endsection
