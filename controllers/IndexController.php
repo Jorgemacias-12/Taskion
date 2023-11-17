1184a6d1fe7c27cf41e903ea9108a8bacce77b9b
Jorge Macias <jorge.macias7905@alumnos.udg.mx>
2023-11-17 06:20:07 -0600

    Update: IndexController
    	In the method view add showHeader key and true as his value

--- controllers/IndexController.php
+++ controllers/IndexController.php
@@ -5,6 +5,6 @@ require_once 'core/Controller.php';
 class IndexController extends Controller {
 
   public function index() {
-    $this->view('index');
+    $this->view('index', ['showHeader' => true]);
   }
 }
