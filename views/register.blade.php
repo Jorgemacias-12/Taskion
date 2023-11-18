e301377be49322f21fe62a13960803c68961a972
Jorge Macias <jorge.macias7905@alumnos.udg.mx>
2023-11-18 02:27:49 -0600

    Add: register view
    	Include html structure, validation and implement funcionality to
    	preview user's avatar.

---
+++ views/register.blade.php
@@ -0,0 +1,92 @@
+@extends('base')
+
+@section('title', 'Registrarse')
+
+@push('styles')
+  <link rel="stylesheet" href="/public/css/register.css">
+@endpush
+
+@push('scripts')
+  <script defer src="/public/js/register.js"></script>
+@endpush
+
+@section('content')
+
+  @if (isset($errors) && is_string($errors))
+    @php
+      $errors = unserialize($errors);
+    @endphp
+  @endif
+
+  <section class="container flex col">
+    <h2 class="title">Registro</h2>
+  </section>
+  <form id="form" class='form flex col' action="/register" method="POST" enctype="multipart/form-data">
+    
+    <section class="form-group">
+      <label class='caption' for="name">Nombre</label>
+      <input class='input' type="text" name="name" id="name" value="{{ $oldInput['name'] }}">
+      @if (isset($errors) && $errors->has('name'))
+        <p class="error">
+          <span class="close fas fa-times"></span>
+          {{ $errors->first('name') }}
+        </p>
+      @endif
+    </section>
+    
+    <section class="form-group">
+      <label class='caption' for="username">Nombre de usuario</label>
+      <input class='input' type="text" name="username" id="username" value="{{ $oldInput['username'] }}">
+      @if (isset($errors) && $errors->has('username'))
+        <p class="error">
+          <span class="close fas fa-times"></span>
+          {{ $errors->first('username') }}
+        </p>
+      @endif
+    </section>
+    
+    <section class="form-group">
+      <label class='caption' for="email">Correo</label>
+      <input class='input' type="text" name="email" id="email" value="{{ $oldInput['email'] }}">
+      @if (isset($errors) && $errors->has('email'))
+        <p class="error">
+          <span class="close fas fa-times"></span>
+          {{ $errors->first('email') }}
+        </p>
+      @endif
+    </section>
+    
+    <section class="form-group">
+      <label class='caption' for="password">Contraseña</label>
+      <input class='input' type="password" name="password" id="password" value="{{ $oldInput['password'] }}">
+      @if (isset($errors) && $errors->has('password'))
+        <p class="error">
+          <span class="close fas fa-times"></span>
+          {{ $errors->first('password') }}
+        </p>
+      @endif
+    </section>
+    
+    <section class="form-group">
+      <label class='caption' for="avatar">Avatar</label>
+      <input class='input-file' type="file" name="avatar" id="avatar" accept="image/png, image/gif, image/jpeg">
+      @if (isset($errors) && $errors->has('avatar'))
+        <p class="error">
+          <span class="close fas fa-times"></span>
+          {{ $errors->first('avatar') }}
+        </p>
+      @endif
+    </section>
+
+    <section id="avatar-container" class="avatar-preview">
+      <img id="avatar-img" class="avatar" src="" alt="">
+    </section>
+
+    <section class="form-group">
+      <button class="button">
+        Registrarme
+      </button>
+    </section>
+
+  </form>
+@endsection
