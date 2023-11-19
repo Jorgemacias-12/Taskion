4d21b510899b55a05a8dc4b34336b38d269d7aac
Jorge Macias <jorge.macias7905@alumnos.udg.mx>
2023-11-19 03:39:57 -0600

    Add: app view
    	This view contains the navigation component
    
    	and two containers that will contain all projects and tasks that
    	are relationated with the current session user

---
+++ views/app.blade.php
@@ -0,0 +1,58 @@
+@extends('base')
+
+@section('title', 'App')
+
+@push('styles')
+  <link rel="stylesheet" href="/public/css/app.css">
+  <link rel="stylesheet" href="/public/css/navigation.css">
+@endpush
+
+@push('scripts')
+  <script defer src="/public/js/app.js"></script>
+@endpush
+
+@php
+  $user = isset($_SESSION['user']) ? unserialize($_SESSION['user']) : null;
+@endphp
+
+@section('navigation')
+  @include('components.navigation')
+@endsection
+
+@section('content')
+
+  <section class="links">
+    <a href="app/projects" class="link">
+      <span class="fas fa-folder"></span>
+      Ir a proyectos
+    </a>
+    <a href="app/tasks" class="link">
+      <span class="fas fa-list"></span>
+      Ir a tareas
+    </a>
+  </section>
+
+  <section class="container projects">
+
+    <h2>
+      Proyectos creados por tí
+    </h2>
+
+    <section id="projects">
+      
+    </section>
+
+  </section>
+
+  <section class="container tasks">
+    <h2>
+      Tareas creadas por tí
+    </h2>
+
+    <section id="tasks">
+
+    </section>
+
+  </section>
+
+@endsection
