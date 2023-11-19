39f40634fc7364b91fba42a05fff69172b3a1f2e
Jorge Macias <jorge.macias7905@alumnos.udg.mx>
2023-11-18 22:39:58 -0600

    Update: login view
    	Add required fields needed to implement login and 	show
    	registration proccess went well

--- views/login.blade.php
+++ views/login.blade.php
@@ -2,6 +2,39 @@
 
 @section('title', 'Iniciar sesión')
 
+@push('styles')
+  <link rel="stylesheet" href="/public/css/form.css">
+@endpush
+
 @section('content')
+
+  <section class="container flex col">
+    <h2 class="title">Login</h2>
+  </section>
+
+  <form action="/login" class="form flex col" method="POST">
+
+    @if (isset($_GET['message']) && $_GET['message'] === "accountCreated")
+      <p class="success">
+        <span class="close fas fa-times"></span>
+        ¡Cuenta creada correctamente!
+      </p>
+    @endif
+
+    <section class="form-group">
+      <label for="" class="caption">Email</label>
+      <input class="input" type="text" name="email" id="email">
+    </section>
+
+    <section class="form-group">
+      <label for="" class="caption">Contraseña</label>
+      <input class="input" type="text" name="password" id="password">
+    </section>
+
+    <section class="form-group">
+      <button class="button" type="submit">Iniciar sesión</button>
+    </section>
+
+  </form>
 
 @endsection
