60a8d95fee20666c5ad31c3961e5b45075d034ab
Jorge Macias <jorge.macias7905@alumnos.udg.mx>
2023-11-19 04:35:44 -0600

    Fix: styles component
    	missing / at the beginning of the URL

--- views/components/styles.blade.php
+++ views/components/styles.blade.php
@@ -1,3 +1,3 @@
-<link rel="stylesheet" href="public/css/initial.css">
-<link rel="stylesheet" href="public/css/header.css">
-<link rel="stylesheet" href="public/css/icons/all.min.css">
+<link rel="stylesheet" href="/public/css/initial.css">
+<link rel="stylesheet" href="/public/css/header.css">
+<link rel="stylesheet" href="/public/css/icons/all.min.css">
