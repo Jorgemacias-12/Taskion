bb26bf8f73a52e78b88c0c5674a0dd90e57653d4
Jorge Macias <jorge.macias7905@alumnos.udg.mx>
2023-11-18 22:38:59 -0600

    Update: base view
    	Add defer attribute to initial.js script

--- views/base.blade.php
+++ views/base.blade.php
@@ -5,7 +5,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <title>Taskion - @yield('title', '')</title>
-  <script src="/public/js/initial.js"></script>
+  <script defer src="/public/js/initial.js"></script>
   @include('components.styles')
 
   @stack('styles')
