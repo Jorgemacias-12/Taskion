95daff309e8ddd264653fa735bc9736e1bbb86d7
Jorge Macias <jorge.macias7905@alumnos.udg.mx>
2023-11-19 15:43:15 -0600

    Update: projects view
    	Remove previous HTML with project creation form
    
    	moved that to project view

--- views/projects.blade.php
+++ views/projects.blade.php
@@ -1,10 +1,11 @@
 @extends('base')
 
-@section('title', 'App')
+@section('title', 'Proyectos')
 
 @push('styles')
   <link rel="stylesheet" href="/public/css/app.css">
   <link rel="stylesheet" href="/public/css/navigation.css">
+  <link rel="stylesheet" href="/public/css/projects.css">
 @endpush
 
 @push('scripts')
@@ -20,5 +21,28 @@
 @endsection
 
 @section('content')
+  <h2 class="title">
+    Proyectos
+  </h2>
+
+  @if (isset($_GET['projectCreated']))
+    <p class="success">
+      <span class="close fas fa-times"></span>
+      ¡Proyecto creado correctamente!
+    </p>
+  @endif
+
+  <section class="links">
+    
+    <a href="projects/create" class="link">
+      <span class="fas fa-plus"></span>
+      Crear proyecto
+    </a>
+
+  </section>
+
+  <section class="flex col" id="projects">
+    {{-- Poner aquí código que cargue todos los proyectos :P --}}
+  </section>
 
 @endsection
