9a24b74a7335d2e29ea20065b8a795fec608203c
Jorge Macias <jorge.macias7905@alumnos.udg.mx>
2023-11-18 22:37:08 -0600

    Update: Model base class
    	Remove anything except for $db attribute

--- core/Model.php
+++ core/Model.php
@@ -5,26 +5,9 @@ require_once './core/Database.php';
 class Model
 {
   protected $db;
-  protected $table;
-  protected $attributes = [];
 
-  public function __construct($table, $attributes)
+  public function __construct()
   {
     $this->db = new Database();
-    $this->table = $table;
-    $this->attributes = $attributes;
   }
-
-  public function save()
-  {
-    $columns = implode(',', array_keys($this->attributes));
-    $placeholders = ":" . implode(', :', array_keys($this->attributes));
-
-    $query = "INSERT INTO {$this->table} ($columns) VALUES ($placeholders)";
-
-    $result = $this->db->executeQuery(false, $query, $this->attributes);
-
-    return $result;
-  }
-
 }
