921665157ad3330adc6909f68d25b71acdb94f9b
Jorge Macias <jorge.macias7905@alumnos.udg.mx>
2023-11-19 04:08:01 -0600

    Update: 404 not found view
    	Move cat image to top of the error element trace

--- views/404.blade.php
+++ views/404.blade.php
@@ -12,10 +12,12 @@
   <h1 class="title">
       404 - Page not found
   </h1>
+
+  <img class="cat" src="https://t3.ftcdn.net/jpg/02/61/08/76/360_F_261087622_8eRI0TAwDAyabS1b0Uifx1wKqHzA41r3.jpg" alt="random cat image">
+  
   <p class="trace">
       Requested URL by user was: /{{ $url }}
       but is not available on this server
   </p>
 
-  <img class="cat" src="https://t3.ftcdn.net/jpg/02/61/08/76/360_F_261087622_8eRI0TAwDAyabS1b0Uifx1wKqHzA41r3.jpg" alt="random cat image">
 @endsection
