2ece61a69bf4f63a9371fde7528f71409fc2896f
Jorge Macias <jorge.macias7905@alumnos.udg.mx>
2023-11-18 22:41:13 -0600

    Update: initial script
    	Now any element that has .close class can remove his father from
    	the DOM

--- public/js/initial.js
+++ public/js/initial.js
@@ -1,3 +1,13 @@
 const $ = (id) => {
-  return document.getElementById(id);
+  return document.getElementById(id)
 }
+
+const closeBtnEls = document.querySelectorAll(".close")
+
+closeBtnEls.forEach((el) => {
+  el.addEventListener("click", (e) => {
+    const errorEl = e.currentTarget.parentNode
+
+    errorEl.parentNode.removeChild(errorEl)
+  })
+})
