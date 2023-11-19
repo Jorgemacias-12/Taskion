c0f52a1da6abc3ed48108bd2cd5ca02c0b082136
Jorge Macias <jorge.macias7905@alumnos.udg.mx>
2023-11-19 03:44:09 -0600

    Add: navigation component
    	Used in app routes it allow to the user see the current user,
    	and his avatar and also provides options like:
    
    	* go to projects
    	* go to tasks
    	* go to edit his profile
    	* logout of the application

---
+++ views/components/navigation.blade.php
@@ -0,0 +1,58 @@
+<header class="header">
+    <section class="header-wrapper">
+
+        <section class="brand-container">
+          <h1>
+            Taskion - Dashboard
+          </h1>
+        </section>
+
+        <section class="user-container flex row">
+
+            <section class="user-info flex row">
+                <p class="username">
+                    {{ $user->getUsername() }} 
+                </p>
+
+                <img class="avatar" title="user avatar" src="data:image/png;base64,{{ $user->getAvatar() }}"
+                    alt="user avatar">
+            </section>
+
+            <section class="user-config-container">
+                <span class="opener fas fa-cog fa-2x"></span>
+                <ul class="dropdown">
+                    <li class="dropdown-item">
+                      <a href="/" class="dropdown-link">
+                        <span class="fas fa-home"></span>
+                        Ir al inicio
+                      </a>
+                    </li>
+                    <li class="dropdown-item">
+                      <a href="" class="dropdown-link">
+                        <span class="fas fa-folder"></span>
+                        Ver proyectos
+                      </a>
+                    </li>
+                    <li class="dropdown-item">
+                      <a href="" class="dropdown-link">
+                        <span class="fas fa-list"></span>
+                        Ver tareas
+                      </a>
+                    </li>
+                    <li class="dropdown-item">
+                      <a href="/app/edit/user" class="dropdown-link">
+                        <span class="fas fa-user"></span>
+                        Editar información
+                      </a>
+                    </li>
+                    <li class="dropdown-item">
+                      <a href="/app?logout" class="dropdown-link">
+                        <span class="fas fa-sign-out-alt"></span>
+                        Cerrar sesión
+                      </a>
+                    </li>
+                </ul>
+            </section>
+        </section>
+    </section>
+</header>
