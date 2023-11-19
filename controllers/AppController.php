1a80ba860fcf283ec4585cf6ddb226ebe29a851a
Jorge Macias <jorge.macias7905@alumnos.udg.mx>
2023-11-19 15:37:57 -0600

    Update: AppController.php
    	- Implemented showProjects method that
    	returns projects view
    
    	- Implemented showProjectForm method that
    	returns create project form view
    
    	- Implemented createProject method that is called by our POST
    	  request in /app/projects/create

--- controllers/AppController.php
+++ controllers/AppController.php
@@ -1,5 +1,7 @@
 <?php
 
+use Illuminate\Support\MessageBag;
+
 require_once 'core/Controller.php';
 
 class AppController extends Controller
@@ -25,6 +27,63 @@ class AppController extends Controller
     $tasks = $this->getsTasksData() ?? [];
 
     $this->view('app', ['showHeader' => false, 'projects' => $projects, 'tasks' => $tasks]);
+  }
+
+  public function showProjects()
+  {
+    if (!isset($_SESSION['user'])) {
+      $this->redirect("login");
+    }
+    
+    $this->view('projects', ['showHeader' => false]);
+  }
+
+  public function showProjectForm()
+  {
+    if (!isset($_SESSION['user'])) {
+      $this->redirect("login");
+    }
+
+    $this->view('project', ['showHeader' => false]);
+  }
+
+  public function createProject()
+  {
+    $user_id = $_POST['user_id'] ?? null;
+
+    $project_name = $_POST['project_name'] ?? null;
+    $project_description = $_POST['project_description'] ?? null;
+    $project_startDate = $_POST['project_startDate'] ?? null;
+    $project_finishDate = $_POST['project_finishDate'] ?? null;
+
+    $errors = new MessageBag();
+
+    if (empty($project_name)) {
+      $errors->add('name', 'El campo de nombre del proyecto es obligatiorio');
+    }
+
+    if (empty($project_description)) {
+      $errors->add('description', 'El campo de descripción del proyecto es obligatirio');
+    }
+
+    if (empty($project_startDate)) {
+      $errors->add('startDate', 'La fecha de inicio es obligatoria.');
+    }
+
+    if (empty($project_finishDate)) {
+      $errors->add('finishDate', 'La fecha de fin es obligatoria');
+    }
+
+    if ($errors->isNotEmpty()) {
+      $this->view('project', ['showHeader' => false, 'errors' => serialize($errors)]);
+      return;
+    }
+
+    $project = new Project($project_name, $project_description, $project_startDate, $project_finishDate, $user_id);
+
+    $project->save();
+
+    $this->redirect('app/projects?projectCreated');
   }
 
   public function getProjectsData()
