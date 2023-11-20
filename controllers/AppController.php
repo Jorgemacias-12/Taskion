ed5b8b40700882dd37ac124f1c7bfaf0911c7cd2
Jorge Macias <jorge.macias7905@alumnos.udg.mx>
2023-11-20 13:51:07 -0600

    Update: AppController
    	- implemented showTaskForm method that show
    	task view
    
    	- implemented createTask method that is responsible of creating
    	  task into the users - tasks  database
    
    	- implemented showTasks method that shows tasks that are
    	  associated with the user

--- controllers/AppController.php
+++ controllers/AppController.php
@@ -29,13 +29,26 @@ class AppController extends Controller
     $this->view('app', ['showHeader' => false, 'projects' => $projects, 'tasks' => $tasks]);
   }
 
+  public function showTasks()
+  {
+    if (!isset($_SESSION['user'])) {
+      $this->redirect("login");
+    }
+
+    $tasks = $this->getsTasksData();
+
+    $this->view('tasks', ['showHeader' => false, 'tasks' => $tasks]);
+  }
+
   public function showProjects()
   {
     if (!isset($_SESSION['user'])) {
       $this->redirect("login");
     }
-    
-    $this->view('projects', ['showHeader' => false]);
+
+    $projects = $this->getProjectsData();
+
+    $this->view('projects', ['showHeader' => false, 'projects' => $projects]);
   }
 
   public function showProjectForm()
@@ -44,7 +57,66 @@ class AppController extends Controller
       $this->redirect("login");
     }
 
-    $this->view('project', ['showHeader' => false]);
+    $this->view('project', ['showHeader' => false,]);
+  }
+
+  public function showTaskForm()
+  {
+    if (!isset($_SESSION['user'])) {
+      $this->redirect("login");
+    }
+
+    $projects = $this->getProjectsData();
+
+    $this->view('task', ['showHeader' => false, 'projects' => serialize($projects)]);
+  }
+
+  public function createTask()
+  {
+    // fetch all projects again
+    $projects = $this->getProjectsData();
+
+    $user_id = $_POST['user_id'] ?? null;
+
+    $project_id = $_POST['project_id'] ?? null;
+
+    $task_name = $_POST['task_name'] ?? null;
+    $task_description = $_POST['task_description'] ?? null;
+    $task_startDate = $_POST['task_startDate'] ?? null;
+    $task_finishDate = $_POST['task_finishDate'] ?? null;
+
+    $errors = new MessageBag();
+
+    if ($project_id === "null") {
+      $errors->add('project', 'Selecciona un proyecto para continuar');
+    }
+
+    if (empty($task_name)) {
+      $errors->add('name', 'El campo de nombre del proyecto es obligatiorio');
+    }
+
+    if (empty($task_description)) {
+      $errors->add('description', 'El campo de descripción de la  tarea es obligatirio');
+    }
+
+    if (empty($task_startDate)) {
+      $errors->add('startDate', 'La fecha de inicio es obligatoria.');
+    }
+
+    if (empty($task_finishDate)) {
+      $errors->add('finishDate', 'La fecha de fin es obligatoria');
+    }
+
+    if ($errors->isNotEmpty()) {
+      $this->view('task', ['showHeader' => false, 'errors' => serialize($errors), 'projects' => serialize($projects)]);
+      return;
+    }
+
+    $task = new Task($task_name, $task_description, $task_startDate, $task_finishDate, $project_id, $user_id);
+
+    $task->save();
+
+    $this->redirect('app/tasks?taskCreated');
   }
 
   public function createProject()
@@ -88,7 +160,9 @@ class AppController extends Controller
 
   public function getProjectsData()
   {
-    return null;
+    $project = new Project();
+
+    return $project->read();
   }
 
   public function getsTasksData()
