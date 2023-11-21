9648b4e44c9e9b174a67fa767fbf7a50fc4bf8f2
Jorge Macias <jorge.macias7905@alumnos.udg.mx>
2023-11-20 18:02:42 -0600

    Update: index.php
    	Improve routing code to detect :id routes

--- index.php
+++ index.php
@@ -10,21 +10,39 @@ $url = filter_var($url, FILTER_SANITIZE_URL);
 $method = $_SERVER['REQUEST_METHOD'];
 
 // Resto del código de enrutamiento...
-if (array_key_exists($url, $routes) && array_key_exists($method, $routes[$url])) {
-  $route = $routes[$url][$method];
-  $controllerName = $route['controller'];
-  $methodName = $route['action'];
+$matched = false;
 
-  require_once "controllers/{$controllerName}.php";
-  $controller = new $controllerName();
-  if (method_exists($controller, $methodName)) {
-    $controller->{$methodName}();
-  } else {
-    // Método no encontrado en el controlador
+foreach ($routes as $routePattern => $routeConfig) {
+    // Reemplaza los marcadores de posición en la ruta con expresiones regulares
+    $routePattern = str_replace(['/', ':id'], ['\/', '([0-9]+)'], $routePattern);
+
+    // Verifica si la URL coincide con el patrón
+    if (preg_match("/^$routePattern$/", $url, $matches) && $method === $routeConfig[$method]['method']) {
+        $matched = true;
+
+        $controllerName = $routeConfig[$method]['controller'];
+        $methodName = $routeConfig[$method]['action'];
+
+        // Elimina el primer elemento que es la URL completa
+        array_shift($matches);
+
+        require_once "./controllers/{$controllerName}.php";
+        $controller = new $controllerName();
+
+        if (method_exists($controller, $methodName)) {
+            // Llama al método y pasa los parámetros capturados
+            call_user_func_array([$controller, $methodName], $matches);
+        } else {
+            // Método no encontrado en el controlador
+            $controller->view('404', ['url' => $url, 'showHeader' => true]);
+        }
+
+        break;
+    }
+}
+
+// Si no hay coincidencias, maneja la ruta no encontrada
+if (!$matched) {
+    $controller = new Controller();
     $controller->view('404', ['url' => $url, 'showHeader' => true]);
-  }
-} else {
-  // Ruta no encontrada o método no permitido
-  $controller = new Controller();
-  $controller->view('404', ['url' => $url, 'showHeader' => true]);
 }
