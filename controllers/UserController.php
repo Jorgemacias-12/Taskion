2deabd2204dacba5e909c11ff15a8fdee158fee6
Jorge Macias <jorge.macias7905@alumnos.udg.mx>
2023-11-24 16:24:44 -0600

    Update: UserController
    	Provide user_id into the projects variable

--- controllers/UserController.php
+++ controllers/UserController.php
@@ -4,9 +4,12 @@ class UserController extends Controller
 {
   public function showUserProfile() {
 
+    // Read user
+    $user = isset($_SESSION['user']) ? unserialize($_SESSION['user']) : null;
+
     // Read projects 
     $projects = new Project();
-    $projects = $projects->read();
+    $projects = $projects->read(null, $user->getId());
 
     // Read tasks
     $tasks = new Task();
