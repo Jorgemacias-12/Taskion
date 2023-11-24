e55092f6fde766f2f8aaaaf5221b070cd9dea70f
Jorge Macias <jorge.macias7905@alumnos.udg.mx>
2023-11-24 01:10:07 -0600

    Update: UserController
    	Implemented `showUserProfile` method that retuns user view of
    	the profile itself
    
    	Implemented `showUserProfileEdit` that will be responsible of
    	show users data into a form

--- controllers/UserController.php
+++ controllers/UserController.php
@@ -3,6 +3,21 @@
 class UserController extends Controller
 {
   public function showUserProfile() {
-    $this->view('user', ['showHeader' => false]);
+
+    // Read projects 
+    $projects = new Project();
+    $projects = $projects->read();
+
+    // Read tasks
+    $tasks = new Task();
+    $tasks = $tasks->read();
+
+    $this->view('user', ['showHeader' => false, 'projects' => $projects, 'tasks' => $tasks]);
   }
+
+  public function showUserProfileEdit() 
+  {
+    $this->view('userEdit', ['showHeader' => false]);
+  }
+
 }
