0c1b75dd168471ee97cf9d2e9357c0a5b1f0c78b
Jorge Macias <jorge.macias7905@alumnos.udg.mx>
2023-11-26 19:51:32 -0600

    Update: base view and styles component
    	Include mc-calendar files

--- views/components/styles.blade.php
+++ views/components/styles.blade.php
@@ -1,3 +1,4 @@
+<link rel="stylesheet" href="/public/css/mc-calendar.min.css">
 <link rel="stylesheet" href="/public/css/initial.css">
 <link rel="stylesheet" href="/public/css/header.css">
 <link rel="stylesheet" href="/public/css/icons/all.min.css">
